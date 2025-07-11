76f96a8019483780048054f48c4d842aa9dfd88a
unknown <vishurudra@gmail.com>
2025-07-11 14:37:45 +0530

    done

--- closure1.js
+++ closure1.js
@@ -1,14 +1,15 @@
 //Closures are way in which we can retain a variable even after the function
 //in which it is defined is done executing
-function UserCounter(startCounter=0){
-    let count =startCounter
+function UserCounter(startCounter = 0) {
+  let count = startCounter;
 
-    return function(){
-        return ++count
-    }
+  return function (inc = true) {
+    if (inc) return ++count;
+    else return --count;
+  };
 }
 
-const noOfUsers =UserCounter(10)
-console.log(noOfUsers())
-console.log(noOfUsers())
-console.log(noOfUsers())
+const noOfUsers = UserCounter(10);
+console.log(noOfUsers());
+console.log(noOfUsers(false));
+console.log(noOfUsers(false));
