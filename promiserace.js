547e48c9ff1139bc1aec832b1176bd5ae5362393
unknown <vishurudra@gmail.com>
2025-07-11 17:27:04 +0530

    done

--- promiserace.js
+++ promiserace.js
@@ -1,5 +1,6 @@
 Promise.race([
-  new Promise((resolve) => setTimeout(() => resolve(10),2000)),
+  new Promise((resolve) => setTimeout(() =>{ console.log("here")
+     resolve(10)},2000)),
   new Promise((resolve,reject) => setTimeout(() => reject(new Error("Error")),1000)),
   new Promise((resolve) => setTimeout(() => resolve(30),100)),
 ]).then((responses)  => console.log(responses));
