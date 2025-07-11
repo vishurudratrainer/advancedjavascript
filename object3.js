2a18d4f25b7922e9f416b2f6fcb815293659600b
unknown <vishurudra@gmail.com>
2025-07-11 10:26:04 +0530

    done

---
+++ object3.js
@@ -0,0 +1,20 @@
+var book= {
+    bookName :"How to C?",
+    authorName:"Yashwant",
+    chapters:[
+        {
+            startPage:1,
+            endPage:10,
+            chapterName:"What is C?"
+        },{
+            startPage:11,
+            endPage:20,
+            chapterName:"variables C?"
+        }
+    ]
+}
+
+console.log(book["bookName"])
+for(let chapter of book.chapters){
+    console.log(chapter["chapterName"])
+}
