1727f2527ed2c946e8cb93d7aba0183565b4094e
unknown <vishurudra@gmail.com>
2025-07-11 10:17:17 +0530

    done

--- objects1.js
+++ objects1.js
@@ -1,8 +1,16 @@
 //Javascript key value pair
 //value scalar/object/function
-var student = {id:1,name:"Vishwa"}
-console.log(student["id"])
-console.log(student["name"])
-console.log(student.id)
-console.log(student.name)
-console.log(JSON.stringify(student))
+var student = {
+  id: 1,
+  name: "Vishwa",
+  display: function () {
+    console.log("Student name is ", this.name);
+    console.log("Student id is ", this.id);
+  },
+};
+console.log(student["id"]);
+console.log(student["name"]);
+console.log(student.id);
+console.log(student.name);
+console.log(JSON.stringify(student));
+student.display()
