c18b0e234d91fdaa66bf0e20c283104ccec3ecef
unknown <vishurudra@gmail.com>
2025-07-11 10:11:11 +0530

    done

---
+++ objects1.js
@@ -0,0 +1,8 @@
+//Javascript key value pair
+//value scalar/object/function
+var student = {id:1,name:"Vishwa"}
+console.log(student["id"])
+console.log(student["name"])
+console.log(student.id)
+console.log(student.name)
+console.log(JSON.stringify(student))
