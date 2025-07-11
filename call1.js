264edcf16f1805c348096b83c09e283a793760f9
unknown <vishurudra@gmail.com>
2025-07-11 12:34:47 +0530

    done

--- call1.js
+++ call1.js
@@ -1,11 +1,11 @@
 //creates a new function when called with this keyword
 
-var display =function(theme){
-    console.log("Display in theme",theme)
+var display =function(theme,location){
+    console.log("Display in theme",theme,location)
     console.log("Emp ID",this.empId,"Emp name",this.empName)
 }
 
 var emp ={empId:10,empName:"Vishwa"}
 
-display.call(emp,"black")
-display.call(emp,["black"])
+display.call(emp,"black","ind")
+display.apply(emp,["black","usa"])
