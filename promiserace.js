c225cb159254b52948d130326252279875e12402
unknown <vishurudra@gmail.com>
2025-07-11 17:24:59 +0530

    done

--- promiserace.js
+++ promiserace.js
@@ -1,5 +1,7 @@
 Promise.race([
   new Promise((resolve) => setTimeout(() => resolve(10),2000)),
-  new Promise((resolve) => setTimeout(() => resolve(20),1000)),
+  new Promise((resolve,reject) => setTimeout(() => reject(new Error("Error")),1000)),
   new Promise((resolve) => setTimeout(() => resolve(30),100)),
 ]).then((responses)  => console.log(responses));
+
+//If error is the last one then others will not be affected
